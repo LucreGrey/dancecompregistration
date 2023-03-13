7ce874d1e90b3761a668dbffbe6b53f5096c4403
Treadfire2013 <Tristanmeaux2013@gmail.com>
2023-03-13 02:51:13 -0600

    Update all

--- main.go
+++ main.go
@@ -2,12 +2,11 @@ package main
 
 import (
 	"net/http"
-	"github.com/lucregrey/dancecompregistration/backend"
 )
 
 func main() {
-	http.HandleFunc("/motownshowdown/", backend.resourceHandler)
-	http.HandleFunc("/loginpage/", backend.resourceHandler)
-	http.HandleFunc("/", backend.viewHandler)
+	http.HandleFunc("/motownshowdown/", resourceHandler)
+	http.HandleFunc("/loginpage/", resourceHandler)
+	http.HandleFunc("/", viewHandler)
 	http.ListenAndServe(":8080", nil)
 }
