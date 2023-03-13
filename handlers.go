1e3e9d580e1d642fca2d09b4e873028089731b5a
Treadfire2013 <Tristanmeaux2013@gmail.com>
2023-03-13 02:51:56 -0600

    Update

---
+++ handlers.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+	"os"
+	"time"
+)
+
+func resourceHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, r.URL.Path[1:])
+}
+
+///////////////////////////////////////////////////
+
+type Page struct {
+	Hostname string
+	Year     string
+}
+
+func loadPage() (*Page, error) {
+	t := time.Now()
+	hostname, err := os.Hostname()
+	if err != nil {
+		panic(err)
+	}
+	return &Page{Hostname: hostname, Year: t.Format("2006")}, nil
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	p, _ := loadPage()
+	t, _ := template.ParseFiles("index.tpl")
+	t.Execute(w, p)
+}
