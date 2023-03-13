2b655839d301faed7f9b0c88568c830c4faca54e
Treadfire2013 <Tristanmeaux2013@gmail.com>
2023-03-13 02:21:16 -0600

    Update all

--- main.go
+++ main.go
@@ -1,27 +1,13 @@
 package main
 
 import (
-	"log"
 	"net/http"
-
 	"github.com/lucregrey/dancecompregistration/backend"
-
-	"github.com/gorilla/mux"
 )
 
 func main() {
-	log.Println("Application Started")
-	router := mux.NewRouter()
-
-	router.HandleFunc("/api/process", backend.ProcessRegistration)
-
-	//router.Host("motownshowdown.dancecompregistration.co").Handler(http.FileServer(http.Dir("motownshowdown.dancecompregistration.com")))
-	router.Host("motownshowdown.localhost").Handler(http.FileServer(http.Dir("motownshowdown.dancecompregistration.com")))
-
-	//router.Host("loginpage.dancecompregistration.com").Handler(http.FileServer(http.Dir("loginpage")))
-	router.Host("loginpage.localhost").Handler(http.FileServer(http.Dir("loginpage")))
-
-	router.PathPrefix("/").Handler(http.FileServer(http.Dir("motownshowdown.dancecompregistration.com")))
-
-	log.Panic(http.ListenAndServe(":80", router))
+	http.HandleFunc("/motownshowdown/", resourceHandler)
+	http.HandleFunc("/loginpage/", resourceHandler)
+	http.HandleFunc("/", viewHandler)
+	http.ListenAndServe(":8080", nil)
 }
---
+++ motownshowdown/js/onload.js
@@ -0,0 +1,17 @@
+window.onload = function() {
+    // Select Dropdown
+    $('html').click(function() {
+        $('.select .dropdown').hide(); 
+    });
+    $('.select').click(function(event){
+        event.stopPropagation();
+    });
+    $('.select .select-control').click(function(){
+        $(this).parent().next().toggle();
+    });
+    $('.select .dropdown li').click(function(){
+        $(this).parent().toggle();
+        var text = $(this).attr('rel');
+        $(this).parent().prev().find('div').text(text);
+    });
+}
