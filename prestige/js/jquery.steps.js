f5900d7523e911d0380db52891989e3b45776bdd
Treadfire2013 <Tristanmeaux2013@gmail.com>
2023-03-25 03:18:47 -0600

    Update

--- prestige/js/jquery.steps.js
+++ prestige/js/jquery.steps.js
@@ -840,7 +840,7 @@ function filterEvents() {
                 console.log("PRO PRO REACHED")
                 for (i in response) {
                     if (i % 2 == 0) {$("#Events").append('<div class="form-row">')}
-                    $("#Events").append('<div class="checkbox-circle mt-6"><label><input type="checkbox" id="' + response[i].Name + '">  ' + response[i].Name + '</a><span class="checkmark"></span></label></div>')
+                    $("#Events").append('<div class="checkbox-circle mt-6"><label><input type="checkbox" id="' + response[Name] + '">  ' + response[Name] + '</a><span class="checkmark"></span></label></div>')
                     if (i % 2 == 0) {$("#Events").append('</div>')}
                 }
             }
