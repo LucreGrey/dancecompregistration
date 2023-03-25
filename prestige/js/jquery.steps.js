1b5afc9e80e8acf648dd5f2369ff5bf9568633a0
Treadfire2013 <Tristanmeaux2013@gmail.com>
2023-03-25 02:54:15 -0600

    Update

--- prestige/js/jquery.steps.js
+++ prestige/js/jquery.steps.js
@@ -840,21 +840,21 @@ function filterEvents() {
                 if ((leadStatus == true) && (followStatus == true)) {
                     for (i in data) {
                         if (i % 2 == 0) {$("#Events").append('<div class="form-row">')}
-                        $("#Events").append('<div class="checkbox-circle mt-6"><label><input type="checkbox" id="' + data[i] + '">  ' + data[i] + '</a><span class="checkmark"></span></label></div>')
+                        $("#Events").append('<div class="checkbox-circle mt-6"><label><input type="checkbox" id="' + data[i].Name + '">  ' + data[i].Name + '</a><span class="checkmark"></span></label></div>')
                         if (i % 2 == 0) {$("#Events").append('</div>')}
                     }
                 }
                 else if (((leadStatus == true) && (followStatus == false)) || ((leadStatus == false) && (followStatus == true))) {
                     for (i in data) {
                         $("#Events").append('<div class="form-row">')
-                        $("#Events").append('<div class="checkbox-circle mt-6"><label><input type="checkbox" id="' + data[i] + '">  ' + data[i] + '</a><span class="checkmark"></span></label></div>')
+                        $("#Events").append('<div class="checkbox-circle mt-6"><label><input type="checkbox" id="' + data[i].Name + '">  ' + data[i].Name + '</a><span class="checkmark"></span></label></div>')
                         $("#Events").append('</div>')
                     }
                 } 
                 else {
                     for (i in data) {
                         if (i % 3 == 0) {$("#Events").append('<div class="form-row">')}
-                        $("#Events").append('<div class="checkbox-circle mt-6"><label><input type="checkbox" id="' + data[i] + '">  ' + data[i] + '</a><span class="checkmark"></span></label></div>')
+                        $("#Events").append('<div class="checkbox-circle mt-6"><label><input type="checkbox" id="' + data[i].Name + '">  ' + data[i].Name + '</a><span class="checkmark"></span></label></div>')
                         if (i % 3 == 0) {$("#Events").append('</div>')}
                     }
                 }
