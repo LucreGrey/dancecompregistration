f7c83d20c1d184c57aeb29ce7349a914cbda7f59
Treadfire2013 <Tristanmeaux2013@gmail.com>
2023-03-25 03:08:37 -0600

    Update

--- prestige/js/jquery.steps.js
+++ prestige/js/jquery.steps.js
@@ -833,7 +833,7 @@ function filterEvents() {
         complete: function(data,error) {
             console.log("DB Called")
             console.log(data)
-            var response = JSON.parse(data)
+            var response = JSON.parse(data.responseText)
             console.log(response)
             if (error) {
                     console.log (error);
