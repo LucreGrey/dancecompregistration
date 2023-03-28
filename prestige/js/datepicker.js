e7217502d6c905b8128d181e5165ded3e133d5b6
Treadfire2013 <Tristanmeaux2013@gmail.com>
2023-03-28 00:00:28 -0600

    Update

--- prestige/js/datepicker.js
+++ prestige/js/datepicker.js
@@ -1,7 +1,24 @@
-$( function() {
-  $( "#datepicker" ).datepicker();
-} );
+import Calendarify from "calendarify"
 
-$( function() {
-    $( "#datepickerf" ).datepicker();
-});
+const calendarify = new Calendarify('.date-picker', {
+  // accent color in hex
+  accentColor: '#0090FC',
+  // enable dark mode
+  isDark: true,
+  // z-index property
+  zIndex: 9999, 
+  // append additional CSS classes to the date picker
+  customClass: ['font-poppins'], 
+  // localize the date picker here
+  locale: {
+    format: "DD-MM-YYYY", // date format
+    lang: {
+      code: 'id', // language id
+      months: [/* month names */], 
+      weekdays: [/* weekdays*/],
+    }
+  }
+  
+})
+calendarify.init();
+
