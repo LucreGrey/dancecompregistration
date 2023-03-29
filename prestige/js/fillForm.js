8b855b34d04c76d2f27f416dfda5f4f6a83df92e
Treadfire2013 <Tristanmeaux2013@gmail.com>
2023-03-28 20:07:15 -0600

    Update More

--- prestige/js/fillForm.js
+++ prestige/js/fillForm.js
@@ -22,24 +22,24 @@ function getInfo() {
 }
 
 function fillForm() {
-    document.getElementById("formlfn").value = Couple.lfn;
-    document.getElementById("formlln").value = Couple.lln;
-    document.getElementById("formdobl").value = Couple.dobl;
-    document.getElementById("formlmp").value = Couple.lmp;
-    document.getElementById("formlndca").value = Couple.lndca;
-    document.getElementById("formlemail").value = Couple.lemail;
+    document.getElementById("formlfn").innerText = Couple.lfn;
+    document.getElementById("formlln").innerText = Couple.lln;
+    document.getElementById("formdobl").innerText = Couple.dobl;
+    document.getElementById("formlmp").innerText = Couple.lmp;
+    document.getElementById("formlndca").innerText = Couple.lndca;
+    document.getElementById("formlemail").innerText = Couple.lemail;
 
-    document.getElementById("formffn").value = Couple.ffn;
-    document.getElementById("formfln").value = Couple.fln;
-    document.getElementById("formdobf").value = Couple.dobf;
-    document.getElementById("formfmp").value = Couple.fmp;
-    document.getElementById("formfndca").value = Couple.fndca;
-    document.getElementById("formfemail").value = Couple.femail;
+    document.getElementById("formffn").innerText = Couple.ffn;
+    document.getElementById("formfln").innerText = Couple.fln;
+    document.getElementById("formdobf").innerText = Couple.dobf;
+    document.getElementById("formfmp").innerText = Couple.fmp;
+    document.getElementById("formfndca").innerText = Couple.fndca;
+    document.getElementById("formfemail").innerText = Couple.femail;
 
-    document.getElementById("formaddr").value = Couple.addr;
-    document.getElementById("formcity").value = Couple.city;
-    document.getElementById("formzip").value = Couple.zip;
-    document.getElementById("formstate").value = Couple.state;
-    document.getElementById("formcountry").value = Couple.country;
-    document.getElementById("formstudio").value = Couple.studio;
+    document.getElementById("formaddr").innerText = Couple.addr;
+    document.getElementById("formcity").innerText = Couple.city;
+    document.getElementById("formzip").innerText = Couple.zip;
+    document.getElementById("formstate").innerText = Couple.state;
+    document.getElementById("formcountry").innerText = Couple.country;
+    document.getElementById("formstudio").innerText = Couple.studio;
 }
