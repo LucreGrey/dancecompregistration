b557d09e460517557584e4869ba7cd80ee7f659a
Treadfire2013 <Tristanmeaux2013@gmail.com>
2023-03-30 17:51:24 -0600

    Update More

--- prestige/js/checkInputs.js
+++ prestige/js/checkInputs.js
@@ -1,6 +1,6 @@
 function checkInputs (str) {
     let try1 = str.search("<")
-    let try2 = str.search("$")
+    let try2 = str.search(/$/)
     if (try1 == -1 && try2 == -1) {
         // Passed the test
         return true
