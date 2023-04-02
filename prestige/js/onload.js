5f23eba7f6d069e438c72a38f55550b3853ae88f
Treadfire2013 <Tristanmeaux2013@gmail.com>
2023-04-02 03:45:16 -0600

    Update More

--- prestige/js/onload.js
+++ prestige/js/onload.js
@@ -14,4 +14,19 @@ window.onload = function() {
         var text = $(this).attr('rel');
         $(this).parent().prev().find('div').text(text);
     });
+
+    var compSelector = document.querySelector('#myDiv');
+
+    var observer = new MutationObserver(function(mutations){
+        mutations.forEach(function(mutation){
+            console.log(mutation.type); // <- It always detects changes
+            if (compSelector.innerHTML == "Prestige Dancesport"){
+                document.getElementById("image-logo").src = "images/PrestigeLogo2.png";
+                document.getElementById("bg-video").src = "./video/Detroit.mp4"
+            } else {
+                document.getElementById("image-logo").src = "dcr.png"
+                document.getElementById("bg-video").src = ""
+            }
+        });    
+    });
 }
