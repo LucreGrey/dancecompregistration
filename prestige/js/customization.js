0a6fcb396126140aa32fdbc1f10f6f1c706b3251
Treadfire2013 <Tristanmeaux2013@gmail.com>
2023-04-02 02:50:48 -0600

    Update More

---
+++ prestige/js/customization.js
@@ -0,0 +1,9 @@
+function customize(attr) {
+    var innerText = attr.innerText;
+    if (innerText = "Prestige Dancesport") {
+        document.getElementById("image-logo").src = "images/PrestigeLogo2.png";
+        document.getElementById("bg-video").src = "./video/Detroit.mp4"
+    } else {
+        document.getElementById("image-logo").src = "dcr.png"
+    }
+}
