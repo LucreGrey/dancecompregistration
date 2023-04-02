4187ed0937f178bc81f025c1d732abc408fab7ef
Treadfire2013 <Tristanmeaux2013@gmail.com>
2023-04-02 03:42:47 -0600

    Update More

--- prestige/js/customization.js
+++ prestige/js/customization.js
@@ -1,11 +1,14 @@
-function customize(attr) {
-    console.log(attr.innerText)
-    var innerText = attr.innerText;
-    if (innerText = "Prestige Dancesport") {
-        document.getElementById("image-logo").src = "images/PrestigeLogo2.png";
-        document.getElementById("bg-video").src = "./video/Detroit.mp4"
-    } else {
-        document.getElementById("image-logo").src = "dcr.png"
-        document.getElementById("bg-video").src = ""
-    }
-}
+var compSelector = document.querySelector('#myDiv');
+
+var observer = new MutationObserver(function(mutations){
+    mutations.forEach(function(mutation){
+        console.log(mutation.type); // <- It always detects changes
+        if (compSelector.innerHTML == "Prestige Dancesport"){
+            document.getElementById("image-logo").src = "images/PrestigeLogo2.png";
+            document.getElementById("bg-video").src = "./video/Detroit.mp4"
+        } else {
+            document.getElementById("image-logo").src = "dcr.png"
+            document.getElementById("bg-video").src = ""
+        }
+    });    
+});
