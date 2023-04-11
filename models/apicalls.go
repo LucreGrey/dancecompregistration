287a95e2dc193fa92acce23217400b1452fa15cd
Treadfire2013 <Tristanmeaux2013@gmail.com>
2023-04-11 01:57:35 -0600

    Update More

--- models/apicalls.go
+++ models/apicalls.go
@@ -3,7 +3,7 @@ package models
 type UpdateEvents struct {
 	LStatus string `json:"leadstatus"`
 	FStatus string `json:"followstatus"`
-	CompId  string `json:"compid"`
+	CompId  int    `json:"compid"`
 }
 
 type CompetitionSelection struct {
