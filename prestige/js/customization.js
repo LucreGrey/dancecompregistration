a1ff991da8fb5f7b6472d652550822d9de3ab2e8
Treadfire2013 <Tristanmeaux2013@gmail.com>
2023-04-10 22:36:47 -0600

    Update More

--- prestige/js/customization.js
+++ prestige/js/customization.js
@@ -1,18 +1,4 @@
-let competition = document.getElementById("competition");
-console.log(competition.innerText)
 
-function updateWebsite() {
-    console.log(document.getElementById("competition").innerText)
-    if (document.getElementById("competition").innerText == "Prestige Dancesport"){
-        console.log("Changed")
-        document.getElementById("image-logo").src = "images/PrestigeLogo2.png";
-        document.getElementById("bg-video").src = "./video/Detroit.mp4"
-    } else {
-        console.log("Not Changed")
-        document.getElementById("image-logo").src = "dcr.png"
-        document.getElementById("bg-video").src = "./video/BGvid2.mp4"
-    }
-}
 
 
 
--- prestige/js/onload.js
+++ prestige/js/onload.js
@@ -14,4 +14,19 @@ window.onload = function() {
         var text = $(this).attr('rel');
         $(this).parent().prev().find('div').text(text);
     });
+
+    let competition = document.getElementById("competition");
+    competition.addEventListener("DOMCharacterDataModified", function(event) {
+        console.log(competition.innerText)
+        if (document.getElementById("competition").innerText == "Prestige Dancesport"){
+            console.log("Changed")
+            document.getElementById("image-logo").src = "images/PrestigeLogo2.png";
+            document.getElementById("bg-video").src = "./video/Detroit.mp4"
+        } else {
+            console.log("Not Changed")
+            document.getElementById("image-logo").src = "dcr.png"
+            document.getElementById("bg-video").src = "./video/BGvid2.mp4"
+        }
+    }, false); 
+
 }
