c073143ec0bd55ad47d08736e4c5715a18d9fb48
Treadfire2013 <Tristanmeaux2013@gmail.com>
2023-04-10 22:19:19 -0600

    Update More

--- prestige/js/customization.js
+++ prestige/js/customization.js
@@ -1,14 +1,16 @@
 let competition = document.getElementById("competition");
 console.log(competition.innerText)
-competition.addEventListener("change", updateSite); 
+competition.addEventListener("DOMCharacterDataModified", updateSite); 
 
 function updateSite(e){ 
     console.log(e)
     console.log(competition.innerText)
-        if (document.getElementById("competition").innerText == "Prestige Dancesport"){
+    if (document.getElementById("competition").innerText == "Prestige Dancesport"){
+        console.log("Changed")
         document.getElementById("image-logo").src = "images/PrestigeLogo2.png";
         document.getElementById("bg-video").src = "./video/Detroit.mp4"
     } else {
+        console.log("Not Changed")
         document.getElementById("image-logo").src = "dcr.png"
         document.getElementById("bg-video").src = "./video/BGvid2.mp4"
     }
