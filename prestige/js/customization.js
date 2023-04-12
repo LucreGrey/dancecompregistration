53dcfc99369d9a8eaaa75f5b576af58afc7fc31a
Treadfire2013 <Tristanmeaux2013@gmail.com>
2023-04-11 22:35:09 -0600

    Update More

--- prestige/js/customization.js
+++ prestige/js/customization.js
@@ -5,18 +5,18 @@ function updateWebsite(){
     currentCompId = document.getElementById("competition").value;
     console.log(currentComp)
     console.log(currentCompId)
+    document.getElementById("Title2").innerText = text;
+    document.getElementById("Title3").innerText = text;
     if (text == "Prestige Dancesport 2023"){
         document.getElementById("image-logo1").src = "images/PrestigeLogo2.png";
         document.getElementById("image-logo2").src = "images/PrestigeLogo2.png";
         document.getElementById("image-logo3").src = "images/PrestigeLogo2.png";
         document.getElementById("bg-video").src = "video/Detroit.mp4";
-        document.getElementById("finalFormHeader").src = text;
 
     } else {
         document.getElementById("image-logo1").src = "images/dcrlogo.png";
         document.getElementById("image-logo2").src = "images/dcrlogo.png";
         document.getElementById("image-logo3").src = "images/dcrlogo.png";
         document.getElementById("bg-video").src = "video/BGvid2.mp4";
-        document.getElementById("finalFormHeader").src = text;
     }
 }
